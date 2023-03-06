34d5dfcc1b807d164b1ae62e5cc2f382fb7c2fc6
Sohag Hasan <sohag.web.dev@gmail.com>
2023-03-06 12:09:37 +0600

    seed fix

--- publishable/database/dummy_seeds/UsersTableSeeder.php
+++ publishable/database/dummy_seeds/UsersTableSeeder.php
@@ -1,29 +1,28 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use TCG\Voyager\Models\Role;
 use TCG\Voyager\Models\User;
+use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
-{
+class UsersTableSeeder extends Seeder {
     /**
      * Auto generated seed file.
      *
      * @return void
      */
-    public function run()
-    {
-        if (User::count() == 0) {
-            $role = Role::where('name', 'admin')->firstOrFail();
+    public function run() {
+        if ( User::count() == 0 ) {
+            $role = Role::where( 'name', 'admin' )->firstOrFail();
 
-            User::create([
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => bcrypt('password'),
-                'remember_token' => Str::random(60),
+            User::create( [
+                'first_name'     => 'Super',
+                'last_name'      => 'Admin',
+                'email'          => 'super@admin.com',
+                'password'       => bcrypt( 'password' ),
+                'remember_token' => Str::random( 60 ),
                 'role_id'        => $role->id,
-            ]);
+            ] );
         }
     }
 }

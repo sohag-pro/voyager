3b64d3d05a9d35ce24533270d30c5d5d748527f3
Sohag Hasan <sohag.web.dev@gmail.com>
2023-03-06 12:01:03 +0600

    migration fix

--- publishable/database/migrations/2016_01_01_000000_create_pages_table.php
+++ publishable/database/migrations/2016_01_01_000000_create_pages_table.php
@@ -14,19 +14,19 @@ class CreatePagesTable extends Migration
     public function up()
     {
         // Create table for storing roles
-        Schema::create('pages', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('author_id');
-            $table->string('title');
-            $table->text('excerpt')->nullable();
-            $table->text('body')->nullable();
-            $table->string('image')->nullable();
-            $table->string('slug')->unique();
-            $table->text('meta_description')->nullable();
-            $table->text('meta_keywords')->nullable();
-            $table->enum('status', Page::$statuses)->default(Page::STATUS_INACTIVE);
-            $table->timestamps();
-        });
+        // Schema::create('pages', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->integer('author_id');
+        //     $table->string('title');
+        //     $table->text('excerpt')->nullable();
+        //     $table->text('body')->nullable();
+        //     $table->string('image')->nullable();
+        //     $table->string('slug')->unique();
+        //     $table->text('meta_description')->nullable();
+        //     $table->text('meta_keywords')->nullable();
+        //     $table->enum('status', Page::$statuses)->default(Page::STATUS_INACTIVE);
+        //     $table->timestamps();
+        // });
     }
 
     /**

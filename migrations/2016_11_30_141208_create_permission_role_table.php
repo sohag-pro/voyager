397be781df5c7a2d3507b90c6407380e90d7f1f2
Sohag Hasan <sohag.web.dev@gmail.com>
2023-03-06 11:45:20 +0600

    migration fix

--- migrations/2016_11_30_141208_create_permission_role_table.php
+++ migrations/2016_11_30_141208_create_permission_role_table.php
@@ -16,7 +16,6 @@ class CreatePermissionRoleTable extends Migration
         Schema::create('permission_role', function (Blueprint $table) {
             $table->bigInteger('permission_id')->unsigned()->index();
             $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
-            $table->bigInteger('role_id')->unsigned()->index();
             $table->foreignUuid('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->primary(['permission_id', 'role_id']);
         });
